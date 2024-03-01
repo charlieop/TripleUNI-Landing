f73563711b7693fef6a19b701b4441f6641011a5
charlieop <spchanae@connect.ust.hk>
2024-03-01 18:54:42 -0500

    created landing, intro and features

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
 <template>
   <main>
-    <HelloWorld msg="This is a Vue3 project build by vite" />
+    <div class="landing-background"></div>
+    <TheLanding />
+    <TheIntroduction />
+    <TheFeatures />
   </main>
 </template>
 
 <script setup>
-import HelloWorld from "@/components/HelloWorld.vue";
+import TheLanding from "@/components/TheLanding.vue";
+import TheIntroduction from "./components/TheIntroduction.vue";
+
+import TheFeatures from "./components/TheFeatures.vue";
 
 document.addEventListener("touchstart", function () {}, true);
 </script>
 
-<style scoped></style>
+<style scoped>
+main {
+  min-height: 500vh;
+}
+
+.landing-background {
+  position: absolute;
+  z-index: -1;
+  inset: 0;
+  height: 115vh;
+  height: 115svh;
+
+  mask-image: linear-gradient(180deg, #ffffff 77%, #ffffff00 100%);
+
+  background-size: 100% 100%;
+  background-position: 0px 0px, 0px 0px, 0px 0px, 0px 0px;
+  background-image: radial-gradient(
+      84% 64% at -28% 91%,
+      #eeb19aff 15%,
+      #f4c39a00 80%
+    ),
+    radial-gradient(82% 60% at 100% 100%, #ffffff 0%, #ffffff00 100%),
+    radial-gradient(142% 56% at 106% -10%, #85daf6ff 8%, #b1e4ea0a 100%),
+    linear-gradient(90deg, #f6afd9ff 6%, #d4aaefff 51%, #b1e4ea 96%);
+}
+</style>
---
+++ src/components/TheFeatures.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <div class="item">
+        <div class="row">
+          <img src="@/assets/imgs/fire.png" alt="" width="100" height="100" />
+          <p>热榜</p>
+        </div>
+        <p class="desc">随时随地, 紧追校园热点</p>
+      </div>
+      <div class="item">
+        <div class="row">
+          <img src="@/assets/imgs/clock.png" alt="" width="100" height="100" />
+          <p>时间轴</p>
+        </div>
+        <p class="desc">不会错过任何新鲜事!</p>
+      </div>
+      <div class="item">
+        <div class="row">
+          <img src="@/assets/imgs/phone.png" alt="" width="100" height="100" />
+          <p>最新评论</p>
+        </div>
+        <p class="desc">大家都在讨论什么👀</p>
+      </div>
+      <h2>
+        想怎么刷?&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp;&nbsp;&nbsp;&nbsp;就这么刷!
+      </h2>
+      <div class="item">
+        <div class="row">
+          <img src="@/assets/imgs/pie.png" alt="" width="100" height="100" />
+          <p>超多分区</p>
+        </div>
+        <p class="desc">找到你感兴趣的话题吧</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from "vue";
+
+const props = defineProps({});
+</script>
+
+<style scoped>
+.wrapper {
+  padding-block: 15vh;
+  padding-block: 15svh;
+  padding-inline: var(--page-padding-inline);
+}
+
+.container {
+  --_row-height: 10rem;
+
+  border: 5px solid black;
+  border-radius: 3rem;
+  padding: 3rem 5rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr 1fr;
+  gap: 1rem;
+}
+
+@media screen and (max-width: 1200px) {
+  .container {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: repeat(3, 1fr);
+  }
+  h2 {
+    grid-row: 2 / 3;
+  }
+  .item {
+    text-align: center;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+img {
+  max-height: var(--_row-height);
+  width: auto;
+  object-fit: contain;
+  object-position: left;
+}
+
+.row {
+  position: relative;
+  display: inline-block;
+  font-weight: 900;
+}
+
+.row p {
+  position: absolute;
+  bottom: 10%;
+  left: 70%;
+  width: max-content;
+  font-size: var(--fs-800);
+}
+
+.desc {
+  font-size: var(--fs-400);
+  margin-left: 1.5ch;
+  margin-block: 1rem;
+}
+
+h2 {
+  grid-column: 1 / 3;
+  text-align: center;
+  font-size: 5em;
+  font-weight: 900;
+}
+</style>
