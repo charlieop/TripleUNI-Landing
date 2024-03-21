d426ed7a0358d3c9239c4c8a0452a8426c38f4db
charlieop <spchanae@connect.ust.hk>
2024-03-21 15:38:37 -0400

    updated for local test

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import "@/assets/styles/resets.css";
-import "@/assets/styles/style.css";
-import "@/assets/styles/fonts.css";
-
-import { createApp } from "vue";
-import App from "./App.vue";
-
-createApp(App).mount("#app");
